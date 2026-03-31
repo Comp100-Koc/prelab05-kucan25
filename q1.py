0467b3e6256f858d46cc482b61e27603e95138c5
kucan25-creator <kucan25@ku.edu.tr>
2026-03-31 11:03:16 +0300

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,5 +1,27 @@
+
+def is_palindromic(n):
+    if n==n[::-1]:
+        return True
+    return False
+
+
+
+
 def longest_palindromic_substring(s):
     """
     Given a string find the longest palindromic substring
     """
-    pass
+    res=''
+    mes=0
+    for i in range(len(s)):
+        for j in range(i,len(s)+1):
+            if s[i:j+1]==s[j:i-1:-1]:
+                if j+1-i>mes:
+                    mes=j+1-i
+                    res=s[i:j+1]
+    if len(res)<=1:
+        return ''
+    else:
+        return res
+                
+            
