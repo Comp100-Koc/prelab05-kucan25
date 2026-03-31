a505748dd4cea88810c3c87e8453b2805c986541
kucan25-creator <kucan25@ku.edu.tr>
2026-03-31 11:45:01 +0300

    Update q3a.py

--- q3a.py
+++ q3a.py
@@ -1,2 +1,30 @@
+def binary_to_decimal(n):
+    n=n[::-1]
+    total=0
+    for i in range(len(n)-2):
+        total+=2**i*int(n[i])
+    return total
+def decimal_to_binary(n):
+    k=0
+    binary=''
+    while 2**k<=n:
+        k+=1
+    k-=1
+    while k>=0:
+        if 2**k>n:
+            binary+='0'
+            k-=1
+        if 2**k<=n:
+            binary+='1'
+            n-=2**k
+            k-=1
+       
+        
+    return binary
+        
+
 def add_binary(a, b):
-    pass
+    total=binary_to_decimal(a)+binary_to_decimal(b)
+    print(total)
+    return '0b'+decimal_to_binary(total)
+    
