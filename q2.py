f1b564230c478bbf363d1bb74691d09176788602
kucan25-creator <kucan25@ku.edu.tr>
2026-03-31 11:13:26 +0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,5 +1,21 @@
+def helper(n):
+    for i in range(len(n)-1):
+        if n[i]==n[i+1]:
+            return True
+    return False
+
+
+
 def remove_adjacent_duplicates(s):
     '''
     Given a string remove all the adjacent duplicate characters and return the string
     '''
-    pass
+    while helper(s):
+        for i in range(len(s)-1):
+            if s[i]==s[i+1]:
+                s=s[:i]+s[i+2:]
+                print(s)
+                break
+    return s
+                
+print(remove_adjacent_duplicates("abbacadd"))
