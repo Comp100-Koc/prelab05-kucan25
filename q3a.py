5fe8cd45818ef6093036aadf3cfb40fbdfd8f189
kucan25-creator <kucan25@ku.edu.tr>
2026-03-31 11:48:37 +0300

    Update q3a.py

--- q3a.py
+++ q3a.py
@@ -2,7 +2,8 @@ def binary_to_decimal(n):
     n=n[::-1]
     total=0
     for i in range(len(n)-2):
-        total+=2**i*int(n[i])
+        if n[i]=='1':
+            total+=2**i
     return total
 def decimal_to_binary(n):
     k=0
@@ -17,14 +18,9 @@ def decimal_to_binary(n):
         if 2**k<=n:
             binary+='1'
             n-=2**k
-            k-=1
-       
-        
+            k-=1      
     return binary
-        
-
 def add_binary(a, b):
     total=binary_to_decimal(a)+binary_to_decimal(b)
-    print(total)
     return '0b'+decimal_to_binary(total)
     
